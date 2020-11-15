e4a44ba002134119f3d5d7520455bbc33c71ff81
EC2 Default User <ec2-user@ip-172-31-73-127.ec2.internal>
2020-11-15 17:23:59 +0000

    adding changes to index file

---
+++ thankyoumessage.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+  <link href="./css/styles.css" rel="stylesheet" type="text/css"/>
+  <script type="text/Javascript" src="./js/script.js"></script>
+  <title>Thank You</title>
+</head>
+
+
+<body>
+<h2>Thank You For Signing Up! Your account has been created!</h2>
+</body>
+
+</html>
